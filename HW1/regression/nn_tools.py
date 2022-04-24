60f6dfb3b0b645d16445bca37d2620411efe624d
jtfreitas <jtfreitaspt@gmail.com>
2022-04-24 19:48:15 +0200

    finished regression

--- HW1/regression/nn_tools.py
+++ HW1/regression/nn_tools.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 class pd_dataset(Dataset):
 
     def __init__(self, df, transform=None):
@@ -16,7 +17,7 @@ class pd_dataset(Dataset):
         self.transform = transform
         # Read the file and split the lines in a list
         self.data = df.values
-        
+
         # Each element of the list self.data is a tuple: (input, output)
 
     def __len__(self):
@@ -39,83 +40,85 @@ class ToTensor(object):
         return (torch.tensor([x]).float(),
                 torch.tensor([y]).float())
 
+
 class training_API(nn.Module):
-    
+
     def __init__(self, device):
         super().__init__()
         super().train()
         super().zero_grad()
-        
+
         self.device = device
 
-    def train_model(self, train_loader, val_loader, num_epochs, loss_fn, optimizer, verbose = True):
+    def train_model(self, train_loader, val_loader, num_epochs, loss_fn, optimizer, verbose=True):
         self.history = dict()
         train_loss_log = []
         val_loss_log = []
         for epoch_num in range(num_epochs):
-
-            ### TRAIN
-            train_loss= []
-            self.train() # Training mode (e.g. enable dropout, batchnorm updates,...)
-            for sample_batched in train_loader:
-                # Move data to device
-                x_batch = sample_batched[0].to(self.device)
-                label_batch = sample_batched[1].to(self.device)
-
-                # Forward pass
-                out = self(x_batch)
-
-                # Compute loss
-                loss = loss_fn(out, label_batch)
-
-                # Backpropagation
-                self.zero_grad()
-                loss.backward()
-
-                # Update the weights
-                optimizer.step()
-
-                # Save train loss for this batch
-                loss_batch = loss.detach().cpu().numpy()
-                train_loss.append(loss_batch)
-
-            train_loss = np.mean(train_loss)
+            train_loss = self.train_step(train_loader, loss_fn, optimizer)
             train_loss_log.append(train_loss)
 
-            ### VALIDATION
-            val_loss= []
-            self.eval() # Evaluation mode (e.g. disable dropout, batchnorm,...)
-            with torch.no_grad(): # Disable gradient tracking
-                for sample_batched in val_loader:
-                    # Move data to device
-                    x_batch = sample_batched[0].to(self.device)
-                    label_batch = sample_batched[1].to(self.device)
+            val_loss = self.evaluate(val_loader, loss_fn, verbose=False)
 
-                    # Forward pass
-                    out = self(x_batch)
+            if verbose:
+                print(
+                    f"Epoch: {epoch_num} >>> Training loss: {train_loss:.5f} | Validation loss: {val_loss:.5f}", end='\r')
+            val_loss_log.append(val_loss)
 
-                    # Compute loss
-                    loss = loss_fn(out, label_batch)
-
-                    # Save val loss for this batch
-                    loss_batch = loss.detach().cpu().numpy()
-                    val_loss.append(loss_batch)
-
-                # Save average validation loss
-                val_loss = np.mean(val_loss)
-                if verbose:
-                    print(f"Epoch: {epoch_num} :::::::::: AVERAGE VAL LOSS: {np.mean(val_loss):.5f}", end = '\r')
-                val_loss_log.append(val_loss)
-                
         self.history['train'] = train_loss_log
         self.history['valid'] = val_loss_log
         self.history['epoch'] = range(num_epochs)
 
-                
+    def train_step(self, dataloader, loss_fn, optimizer):
+        train_loss = []
+        self.train()  # Training mode (e.g. enable dropout, batchnorm updates,...)
+        for sample_batched in dataloader:
+            # Move data to device
+            x_batch = sample_batched[0].to(self.device)
+            label_batch = sample_batched[1].to(self.device)
+
+            # Forward pass
+            out = self(x_batch)
+
+            # Compute loss
+            loss = loss_fn(out, label_batch)
+
+            # Backpropagation
+            self.zero_grad()
+            loss.backward()
+
+            # Update the weights
+            optimizer.step()
+
+            # Save train loss for this batch
+            loss_batch = loss.detach().cpu().numpy()
+            train_loss.append(loss_batch)
+
+        train_loss = np.mean(train_loss)
+        return train_loss
+
+    def evaluate(self, dataloader, loss_fn, verbose=True):
+        self.eval()
+        data_loss = []
+        with torch.no_grad():
+            for sample_batched in dataloader:
+                x_batch = sample_batched[0].to(self.device)
+                label_batch = sample_batched[1].to(self.device)
+
+                out = self(x_batch)
+                loss = loss_fn(out, label_batch)
+                loss_batch = loss.detach().cpu().numpy()
+                data_loss.append(loss_batch)
+
+            data_loss = np.mean(data_loss)
+            if verbose:
+                print(f"Loss = {data_loss}")
+            return data_loss
+
     def write_validation(self, val_loader, verbose, epoch_num):
-        val_loss= []
-        self.eval() # Evaluation mode (e.g. disable dropout, batchnorm,...)
-        with torch.no_grad(): # Disable gradient tracking
+        val_loss = []
+        self.eval()  # Evaluation mode (e.g. disable dropout, batchnorm,...)
+        with torch.no_grad():  # Disable gradient tracking
             for sample_batched in val_loader:
                 # Move data to device
                 x_batch = sample_batched[0].to(self.device)
@@ -130,24 +133,10 @@ class training_API(nn.Module):
             # Save average validation loss
             val_loss = np.mean(val_loss)
             if verbose:
-                print(f"Epoch: {epoch_num} :::::::::: AVERAGE VAL LOSS: {np.mean(val_loss):.5f}", end = '\r')
+                print(
+                    f"Epoch: {epoch_num} :::::::::: AVERAGE VAL LOSS: {np.mean(val_loss):.5f}", end='\r')
         return val_loss
-        
-    def class_accuracy(self, dataloader):
-        
-        self.eval() # Evaluation mode (e.g. disable dropout, batchnorm,...)
-        with torch.no_grad(): # Disable gradient tracking
-            for sample_batched in dataloader:
-                # Move data to device
-                x_batch = sample_batched[0].to(self.device)
-                label_batch = sample_batched[1].to(self.device)
-                # Forward pass
-                out = self(x_batch)
-                print(out)
-               # accuracy = accuracy_score(label_batch, out, normalize = True)
-            
-        return accuracy
-    
+
     def predict(self, datapoint, activation, *args, **kwargs):
 
         with torch.no_grad():
@@ -155,10 +144,10 @@ class training_API(nn.Module):
             out = self(x_val)
             predicted_label = activation(out, *args, **kwargs)
         return predicted_label
-            
+
 
 class reg_model(training_API):
-    
+
     def __init__(self, N_input, N_h1, N_h2, N_h3, N_out, device):
         """
         n_input - Input size
@@ -169,18 +158,17 @@ class reg_model(training_API):
         super().__init__(device)
 
         print('Network initialized')
-        
-        self.fc1 = nn.Linear(in_features = N_input, out_features = N_h1)
-        self.fc2 = nn.Linear(in_features = N_h1, out_features = N_h2)
-        self.fc3 = nn.Linear(in_features = N_h2, out_features = N_h3)
-        self.out = nn.Linear(in_features = N_h3, out_features = N_out)
+
+        self.fc1 = nn.Linear(in_features=N_input, out_features=N_h1)
+        self.fc2 = nn.Linear(in_features=N_h1, out_features=N_h2)
+        self.fc3 = nn.Linear(in_features=N_h2, out_features=N_h3)
+        self.out = nn.Linear(in_features=N_h3, out_features=N_out)
         self.act = nn.Sigmoid()
 
     def forward(self, x):
-        
+
         x = self.act(self.fc1(x))
         x = self.act(self.fc2(x))
         x = self.act(self.fc3(x))
         x = self.out(x)
         return x
-    
